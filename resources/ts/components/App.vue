b36c6ca015809f49f5d15dd86e94faa684bdcb80
Ian Torres <iantorres@outlook.com>
2025-11-21 18:29:25 -0300

    Frontend stuffs added.

--- resources/ts/components/App.vue
+++ resources/ts/components/App.vue
@@ -1,5 +1,9 @@
 <template>
-    <slot />
+    <router-view v-slot="{ Component, route }">
+        <div :key="route.name">
+            <Component :is="Component" />
+        </div>
+    </router-view>
 </template>
 
 <script lang="ts" setup></script>
